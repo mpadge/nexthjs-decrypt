dd13400424da2df36b370a53921ca0b9d518c5a8
mpadge <mark.padgham@email.com>
2024-01-16 13:40:42 +0100

    separate getSymmetricKey fn in route.tsx

--- src/app/api/route.tsx
+++ src/app/api/route.tsx
@@ -1,15 +1,20 @@
 import * as crypto from 'crypto';
 
+function getSymmetricKey(): crypto.CipherKey {
+    const symKeyBase64: string = process.env.SYMMETRIC_KEY || '';
+
+    const symKeyBuffer = Buffer.from(symKeyBase64, 'base64');
+    if (symKeyBuffer.length !== 16) {
+        throw new Error('Invalid key length: ' + symKeyBuffer.length);
+    }
+
+    return symKeyBuffer as crypto.CipherKey;
+}
+
 export async function POST(request: any): Promise<Response> {
    return new Promise(async (resolve, reject) => {
 
-       const symKeyBase64: string = process.env.SYMMETRIC_KEY || '';
-
-       const symKeyBuffer = Buffer.from(symKeyBase64, 'base64');
-       if (symKeyBuffer.length !== 16) {
-           reject(new Error('Invalid key length: ' + symKeyBuffer.length));
-           return;
-       }
+       const key = getSymmetricKey();
 
        const algorithm = 'aes-128-cbc';
        const clearEncoding: crypto.BinaryToTextEncoding = 'binary';
@@ -19,7 +24,7 @@ export async function POST(request: any): Promise<Response> {
        const arrayBuffer = await request.arrayBuffer();
        const encryptedData = Buffer.from(arrayBuffer, 'binary');
 
-       var decipher = crypto.createDecipheriv(algorithm, symKeyBuffer, iv);
+       var decipher = crypto.createDecipheriv(algorithm, key, iv);
        var decryptedData = [];
        decryptedData.push(decipher.update(encryptedData as any, cipherEncoding, clearEncoding));
        decryptedData.push(decipher.final());
